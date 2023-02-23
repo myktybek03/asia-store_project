4a44015464f97f540f10e4a49eef69a4a31f3cfd
Myktybek <admin@Admins-MacBook-Pro.local>
2023-02-23 12:29:46 +0600

    structure-project

--- src/components/UI/modal/BasicModal.jsx
+++ src/components/UI/modal/BasicModal.jsx
@@ -1,15 +1,14 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import { styled } from '@mui/system';
+import * as React from "react"
+import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import Modal from "@mui/material/Modal"
+import { styled } from "@mui/system"
 
-
-export default function BasicModal({children}) {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+export default function BasicModal({ children }) {
+  const [open, setOpen] = React.useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
 
   return (
     <div>
@@ -21,34 +20,37 @@ export default function BasicModal({children}) {
         aria-describedby="modal-modal-description"
       >
         <BoxStyle>
-          <TypographyStyle id="modal-modal-title" variant="h6" component="h2">
-          </TypographyStyle>
-          <TypographyStyle id="modal-modal-description">
-          </TypographyStyle>
+          <TypographyStyle
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+          ></TypographyStyle>
+          <TypographyStyle id="modal-modal-description"></TypographyStyle>
           {children}
         </BoxStyle>
       </Modal>
     </div>
-  );
+  )
 }
 
-const BoxStyle = styled(Box)(() =>({
-  "&":{
-    position: "absolute" ,
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+const BoxStyle = styled(Box)(() => ({
+  "&": {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
     width: "700px",
     height: "500px",
-    backgroundColor: 'white',
-    border: '2px solid green', 
+    backgroundColor: "white",
+    border: "2px solid green",
     borderRadius: 20,
     boxShadow: 24,
     p: 4,
-  }
+  },
 }))
 
-const TypographyStyle = styled(Typography)(() =>({
-  '&': {
-    mt: 2
-}}))
+const TypographyStyle = styled(Typography)(() => ({
+  "&": {
+    mt: 2,
+  },
+}))
